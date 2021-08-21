b286caa91e6bbcaf2ba1745ce9419ae0fdfdd0e8
Brian Haddad <brianhaddad@gmail.com>
2021-08-21 16:45:13 -0700

    Preparing to do something real with this. Added the Moobie DTO.

---
+++ MoobieGoobie/Moobie.WebView/DTOs/Moobie.cs
@@ -0,0 +1,17 @@
+﻿namespace Moobie.WebView.DTOs
+{
+    public class Moobie
+    {
+        public string Alphabetize { get; set; }
+        public string Title { get; set; }
+        public int RuntimeMinutes { get; set; }
+        public string Rating { get; set; }
+        public bool Favorite { get; set; }
+        public string Notes { get; set; }
+        public string BestFormat { get; set; }
+        public string Digital { get; set; }
+        public bool Dvd { get; set; }
+        public bool BluRay { get; set; }
+        public bool FourK { get; set; }
+    }
+}
